cf363e375f21f95f7e6de6ad14e04d460bf85bd3
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-06-18 00:00:13 +0100

    set up database using prisma

--- src/env.mjs
+++ src/env.mjs
@@ -9,6 +9,7 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     OPENAI_API_KEY: z.string(),
+    DATABASE_URL: z.string(),
   },
 
   /**
@@ -27,6 +28,7 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
