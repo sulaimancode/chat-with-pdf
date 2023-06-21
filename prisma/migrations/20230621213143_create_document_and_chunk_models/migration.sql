468ffa79cdd1e121ddbf710340c4203d09c5d4e9
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-06-21 22:33:09 +0100

    create Document and Chunks tables

---
+++ prisma/migrations/20230621213143_create_document_and_chunk_models/migration.sql
@@ -0,0 +1,26 @@
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "vector" WITH SCHEMA "extensions";
+
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chunk" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "tokenCount" INTEGER NOT NULL,
+    "embedding" vector NOT NULL,
+    "documentId" TEXT NOT NULL,
+
+    CONSTRAINT "Chunk_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Chunk" ADD CONSTRAINT "Chunk_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
