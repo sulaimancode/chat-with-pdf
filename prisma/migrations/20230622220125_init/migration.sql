059b169d715071a51a886f8d08d5bb2adcd84fac
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-06-22 23:02:42 +0100

    use proper naming convention for tables

--- prisma/migrations/20230622220125_init/migration.sql
+++ prisma/migrations/20230622220125_init/migration.sql
@@ -1,26 +1,26 @@
 -- CreateExtension
-CREATE EXTENSION IF NOT EXISTS "vector" WITH SCHEMA "extensions";
+CREATE EXTENSION IF NOT EXISTS "vector";
 
 -- CreateTable
-CREATE TABLE "Document" (
+CREATE TABLE "document" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" TEXT NOT NULL,
 
-    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "document_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Chunk" (
+CREATE TABLE "chunk" (
     "id" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "tokenCount" INTEGER NOT NULL,
     "embedding" vector,
     "documentId" TEXT NOT NULL,
 
-    CONSTRAINT "Chunk_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "chunk_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "Chunk" ADD CONSTRAINT "Chunk_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "chunk" ADD CONSTRAINT "chunk_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "document"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
